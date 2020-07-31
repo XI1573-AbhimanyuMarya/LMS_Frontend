e47174f54ecf95381bac1b3abdfe68bd508fcb4d
Vishnu Kumar <Vishnu.Kumar@XEBIAINDIA.LOCAL>
2020-07-31 18:20:03 +0530

    added logout functionality

---
+++ src/components/User/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export const useStyles = makeStyles(theme => ({
+    avatar: {
+        marginRight: '.6em',
+    },
+    notification: {
+        marginRight: '1.1em'
+    },
+    typography: {
+        fontWeight: 600,
+        fontSize: 12
+    },
+    iconButton: {
+        padding: '.5em'
+    }
+}))
--- src/store/actions/loginActions.js
+++ src/store/actions/loginActions.js
@@ -4,9 +4,11 @@ import { actionTypes } from '../types';
 const getUserDetails = (name, value) => ({ type: actionTypes.GET_USER_DETAILS, payload: { name, value } });
 const validateUserEmail = username => ({ type: actionTypes.FETCH_OTP_REQUEST, payload: username });
 const velidateOtp = (username, password) => ({ type: actionTypes.LOGIN_CALL_REQUEST, payload: { username, password } });
+const logout = () => ({ type: actionTypes.LOGOUT_USER_REQUEST });
 
 export default {
     getUserDetails,
     validateUserEmail,
     velidateOtp,
+    logout,
 }
