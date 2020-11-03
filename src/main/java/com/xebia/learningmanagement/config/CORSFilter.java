74df6b8ecad4db18278e5f43f40ccfe6a8d86f5b
Arka-Bandyopadhyay-xebia <73871197+Arka-Bandyopadhyay-xebia@users.noreply.github.com>
2020-11-04 00:38:18 +0530

    removing global cors config

--- src/main/java/com/xebia/learningmanagement/config/CORSFilter.java
+++ src/main/java/com/xebia/learningmanagement/config/CORSFilter.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 //@Configuration
-@Component
 public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         System.out.println("Filtering on...........................................................");
