c12c6c5e1f618801c5a3c1978251481902025037
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-06 21:24:43 +0530

    Send Email using Interface: FreeMarkerConfig

--- src/main/java/com/xebia/learningmanagement/util/EmailSend.java
+++ src/main/java/com/xebia/learningmanagement/util/EmailSend.java
@@ -1,13 +1,44 @@
 package com.xebia.learningmanagement.util;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
+import javax.annotation.PostConstruct;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.util.Date;
+import java.util.Map;
 import java.util.Properties;
 
+@Slf4j
+@Service
 public class EmailSend {
+    private Session session;
+    @Autowired
+    private Configuration freemarkerConfig;
+    @Value("${email.mailPassword}")
+    private String mailPassword;
+    @Value("${email.mailHost}")
+    private String mailHost;
+    @Value("${email.mailSmtpAuth}")
+    private String mailSmtpAuth;
+    @Value("${email.mailSmtpStartTlsEnable}")
+    private String mailSmtpStartTlsEnable;
+    @Value("${email.emailFrom}")
+    private String emailFrom;
+    @Value("${email.mailPort}")
+    private String mailPort;
+
     public static void setEmail(String recipient, String subject, String text) {
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(recipient);
@@ -46,5 +77,55 @@ public class EmailSend {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    @PostConstruct
+    public void init() {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", mailSmtpAuth);
+        props.put("mail.smtp.starttls.enable", mailSmtpStartTlsEnable);
+        props.put("mail.smtp.host", mailHost);
+        props.put("mail.smtp.port", mailPort);
+        this.session = Session.getInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailFrom, mailPassword);
+            }
+        });
+    }
+
+    public void sendEmailMethod(Map<String, String> modelContent) throws Exception {
+        log.info("In user creation sendMailUsingMime method..");
+
+        try {
+            Message message = new MimeMessage(this.session);
+            message.setFrom(new InternetAddress(emailFrom));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(modelContent.get("Email")));
+            message.setSubject(getTemplateValue("subject.ftl", modelContent));
+            message.setSentDate(new Date());
+            MimeMultipart multipart = new MimeMultipart("related");
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(getTemplateValue("body.ftl", modelContent),
+                    "text/html");
+            multipart.addBodyPart(messageBodyPart);
+            message.setContent(multipart);
+            Transport transport = session.getTransport("smtp");
+            transport.connect();
+            transport.sendMessage(message, message.getAllRecipients());
+            log.info("Sent Mail Successfully");
+        } catch (Exception e) {
+            log.error("Excepton ", e);
+            throw new Exception("Email server issue");
+        }
+    }
+
+    private String getTemplateValue(String template, Map<String, String> properties) throws Exception {
+        log.info("Getting template: " + template);
+        freemarkerConfig.setDirectoryForTemplateLoading(new File("C:/Java Projects/Learning Management System/Learning/src/main/resources/templates"));
+        Template template1 = freemarkerConfig.getTemplate(template);
+
+        Template t = freemarkerConfig.getTemplate(template);
+        return FreeMarkerTemplateUtils.processTemplateIntoString(template1, properties);
     }
 }
