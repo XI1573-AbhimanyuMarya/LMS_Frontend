c6aa3834f3c2e7b5095731fd249d040320c2ccd6
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-15 01:51:27 +0530

    Query DB by manager Username

--- src/main/java/com/xebia/learningmanagement/service/LearningPathService.java
+++ src/main/java/com/xebia/learningmanagement/service/LearningPathService.java
@@ -1,10 +1,10 @@
 package com.xebia.learningmanagement.service;
 
 import com.xebia.learningmanagement.model.LearningPathDto;
-
-import static org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import com.xebia.learningmanagement.model.ManagerUsernameDto;
 
 public interface LearningPathService {
 
     void createLearningPath(LearningPathDto.Path path) throws Exception;
+    void getAllAssignedLearningPath(ManagerUsernameDto managerUsernameDto);
 }
