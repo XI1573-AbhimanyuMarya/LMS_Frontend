884a83a5e420218764907a42c6d60ecbddb4abeb
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-16 10:52:42 +0530

    Dto's

--- src/main/java/com/xebia/learningmanagement/model/CategoryDto.java
+++ src/main/java/com/xebia/learningmanagement/model/CategoryDto.java
@@ -1,40 +1,11 @@
 package com.xebia.learningmanagement.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CategoryDto {
-    private Long categoryId;
-    private String code;
+    private String id;
     private String name;
-//	private CompentencyHead compentencyHead;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-//	public CompentencyHead getCompentencyHead() {
-//		return compentencyHead;
-//	}
-//
-//	public void setCompentencyHead(CompentencyHead compentencyHead) {
-//		this.compentencyHead = compentencyHead;
-//	}
 }
