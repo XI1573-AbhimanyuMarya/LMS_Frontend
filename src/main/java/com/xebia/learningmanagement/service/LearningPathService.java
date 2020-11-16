5e205d580ebfc12d792ffc7ce9287b9361971d9a
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-17 01:31:25 +0530

    Refactor

--- src/main/java/com/xebia/learningmanagement/service/LearningPathService.java
+++ src/main/java/com/xebia/learningmanagement/service/LearningPathService.java
@@ -1,11 +1,11 @@
 package com.xebia.learningmanagement.service;
 
 import com.xebia.learningmanagement.model.LearningPathDto;
-import com.xebia.learningmanagement.model.LearningPathListListDto;
+import com.xebia.learningmanagement.model.ListOfLearningPathAssignedDto;
 import com.xebia.learningmanagement.model.ManagerUsernameDto;
 
 public interface LearningPathService {
 
     void createLearningPath(LearningPathDto.Path path) throws Exception;
-    LearningPathListListDto getAllAssignedLearningPath(ManagerUsernameDto managerUsernameDto);
+    ListOfLearningPathAssignedDto getAllAssignedLearningPath(ManagerUsernameDto managerUsernameDto);
 }
