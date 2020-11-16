5f920d0034bbb673b80358a344a83da4cc4e2a48
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-17 01:29:51 +0530

    CategoryDto

--- src/main/java/com/xebia/learningmanagement/model/CoursesDto.java
+++ src/main/java/com/xebia/learningmanagement/model/CoursesDto.java
@@ -3,12 +3,12 @@ package com.xebia.learningmanagement.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class CoursesDto {
     private String name;
     private String description;
-    private List<CategoryDto> category;
+    private CategoryDto category;
+
+
 }
