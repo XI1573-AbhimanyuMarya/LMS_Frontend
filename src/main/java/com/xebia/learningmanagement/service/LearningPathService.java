1b5f4bba6d19890bbe27bb9240cf719f6b90a62a
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-16 10:51:20 +0530

    Define method in interface

--- src/main/java/com/xebia/learningmanagement/service/LearningPathService.java
+++ src/main/java/com/xebia/learningmanagement/service/LearningPathService.java
@@ -1,10 +1,11 @@
 package com.xebia.learningmanagement.service;
 
 import com.xebia.learningmanagement.model.LearningPathDto;
+import com.xebia.learningmanagement.model.LearningPathListListDto;
 import com.xebia.learningmanagement.model.ManagerUsernameDto;
 
 public interface LearningPathService {
 
     void createLearningPath(LearningPathDto.Path path) throws Exception;
-    void getAllAssignedLearningPath(ManagerUsernameDto managerUsernameDto);
+    LearningPathListListDto getAllAssignedLearningPath(ManagerUsernameDto managerUsernameDto);
 }
