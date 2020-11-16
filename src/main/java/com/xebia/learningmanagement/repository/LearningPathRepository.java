81adb847ab5705a5958688ef41bc87511802a4ca
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-16 12:28:36 +0530

    Repo

--- src/main/java/com/xebia/learningmanagement/repository/LearningPathRepository.java
+++ src/main/java/com/xebia/learningmanagement/repository/LearningPathRepository.java
@@ -1,12 +1,9 @@
 package com.xebia.learningmanagement.repository;
 
-import com.xebia.learningmanagement.entity.Category;
 import com.xebia.learningmanagement.entity.LearningPath;
-import com.xebia.learningmanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LearningPathRepository extends JpaRepository<LearningPath,Long> {
+public interface LearningPathRepository extends JpaRepository<LearningPath, Long> {
 }
