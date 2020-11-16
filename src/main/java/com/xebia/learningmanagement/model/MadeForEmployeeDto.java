81defadf2c97a09d2cec4edb86f9f24374b90b97
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-17 01:30:43 +0530

    EmployeeDto

--- src/main/java/com/xebia/learningmanagement/model/MadeForEmployeeDto.java
+++ src/main/java/com/xebia/learningmanagement/model/MadeForEmployeeDto.java
@@ -3,14 +3,12 @@ package com.xebia.learningmanagement.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class MadeForEmployeeDto {
 
     private Long id;
     private int percentCompleted;
-    private List<EmployeeDto> employee;
+    private EmployeeDto employee;
 
 }
