f5e8d8cef10c9fc2f3131cadc8c83a57610483e5
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-18 12:10:08 +0530

    Employee email request dto

---
+++ src/main/java/com/xebia/learningmanagement/dtos/request/EmployeeEmailRequest.java
@@ -0,0 +1,12 @@
+package com.xebia.learningmanagement.dtos.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeEmailRequest {
+    public String employeeEmail;
+}
