712f2bb2c3d3cbaa1e08964b7f1df79ac3a1acf5
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-19 02:38:55 +0530

    Start-End Date in Dtos

--- src/main/java/com/xebia/learningmanagement/dtos/LearningPathManagerDto.java
+++ src/main/java/com/xebia/learningmanagement/dtos/LearningPathManagerDto.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -16,5 +17,7 @@ public class LearningPathManagerDto {
     private List<MadeForEmployeeDto> madeFor;
     private List<CoursesListDto> courses;
     private DurationDto duration;
+    private LocalDate startDate;
+    private LocalDate endDate;
 
 }
