8198b87405edbce0779eaef57f258621b5ec4cbf
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-18 15:32:59 +0530

    Employee LearningPath Service

--- src/main/java/com/xebia/learningmanagement/service/EmployeeLearningPathService.java
+++ src/main/java/com/xebia/learningmanagement/service/EmployeeLearningPathService.java
@@ -1,12 +1,16 @@
 package com.xebia.learningmanagement.service;
 
+import com.xebia.learningmanagement.dtos.EmployeeLearningPathStatisticsDto;
 import com.xebia.learningmanagement.dtos.request.EmployeeEmailRequest;
 import com.xebia.learningmanagement.exception.LearningPathException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
+
 @Service
 public interface EmployeeLearningPathService {
     void deleteLearningPath(Map data) throws LearningPathException;
-    void getMyAssignedLearningPaths(EmployeeEmailRequest employeeEmail) throws LearningPathException;
+
+    List<EmployeeLearningPathStatisticsDto> getMyAssignedLearningPaths(EmployeeEmailRequest employeeEmail) throws LearningPathException;
 }
