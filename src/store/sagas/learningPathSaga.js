dbfe2227e162a06950c1f8191277db7594507562
Shivendra Bind <Shivendra.Bind@XEBIAINDIA.LOCAL>
2020-11-18 20:06:22 +0530

    added flow to get  manager/employee learning path

--- src/store/sagas/learningPathSaga.js
+++ src/store/sagas/learningPathSaga.js
@@ -5,21 +5,23 @@ import { SERVICE_URLS } from '../../modules/constants';
 import { authHeader } from '../../modules/authServices';
 
 const fetchAllCourses = async () => {
-    return await axios.get(SERVICE_URLS.FETCH_COURSES, { headers: authHeader()});
+    return await axios.get(SERVICE_URLS.FETCH_COURSES, { headers: authHeader() });
 }
 
 const fetchAllUsers = async () => {
-    return await axios.get(SERVICE_URLS.FETCH_USERS, { headers: authHeader()});
+    return await axios.get(SERVICE_URLS.FETCH_USERS, { headers: authHeader() });
 }
 
-const createLearningPath = async ({path}) => {
-    return await axios.post(SERVICE_URLS.CREATE_LEARNING_PATH, { path }, { headers: authHeader()});
+const createLearningPath = async ({ path }) => {
+    return await axios.post(SERVICE_URLS.CREATE_LEARNING_PATH, { path }, { headers: authHeader() });
 }
 
 export function* learningPathSaga() {
     yield takeLatest(actionTypes.FETCH_COURSES_REQUEST, fetchCourses);
     yield takeLatest(actionTypes.FETCH_USERS_REQUEST, fetchUsers);
     yield takeLatest(actionTypes.CREATE_LEARNING_PATH_CALL_REQUEST, createLearning);
+    yield takeLatest(actionTypes.GET_ASSIGNED_LEARNING_PATH_REQUEST, getAssignedLearningPaths);
+    yield takeLatest(actionTypes.GET_MY_LEARNING_PATH_REQUEST, getMyLearningPath);
 }
 
 function* fetchCourses() {
@@ -56,5 +58,35 @@ function* createLearning(action) {
         const { response } = error;
         const { data } = response
         yield put({ type: actionTypes.CREATE_LEARNING_PATH_CALL_FAILURE, payload: data });
+    }
+}
+
+const fetchPath = async ({ managerEmail }) => {
+    return await axios.post(SERVICE_URLS.ASSIGNED_PATH, { managerEmail }, { headers: authHeader() });
+}
+
+function* getAssignedLearningPaths(action) {
+    try {
+        const response = yield call(fetchPath, action.payload);
+        const { data } = response;
+        console.log(data);
+        yield put({ type: actionTypes.GET_ASSIGNED_LEARNING_PATH_SUCCESS, payload: data });
+
+    } catch (error) {
+        yield put({ type: actionTypes.GET_ASSIGNED_LEARNING_PATH_FAILURE, payload: error });
+    }
+}
+const fetchMyPath = async ({ employeeEmail }) => {
+    return await axios.post(SERVICE_URLS.MY_PATH, { employeeEmail }, { headers: authHeader() });
+}
+
+function* getMyLearningPath(action) {
+    try {
+        const response = yield call(fetchMyPath, action.payload);
+        const { data } = response;
+        yield put({ type: actionTypes.GET_MY_LEARNING_PATH_SUCCESS, payload: data });
+
+    } catch (error) {
+        yield put({ type: actionTypes.GET_MY_LEARNING_PATH_FAILURE, payload: error });
     }
 }
