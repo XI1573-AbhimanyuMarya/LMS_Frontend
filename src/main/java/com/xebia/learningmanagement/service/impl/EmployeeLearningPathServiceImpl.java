ad55ee44343389c283f3a8f1d050efb6737c5aaf
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-18 15:33:58 +0530

    get Employee Assigned LearningPaths

--- src/main/java/com/xebia/learningmanagement/service/impl/EmployeeLearningPathServiceImpl.java
+++ src/main/java/com/xebia/learningmanagement/service/impl/EmployeeLearningPathServiceImpl.java
@@ -1,13 +1,14 @@
 package com.xebia.learningmanagement.service.impl;
 
+import com.xebia.learningmanagement.dtos.EmployeeLearningPathStatisticsDto;
 import com.xebia.learningmanagement.dtos.request.EmployeeEmailRequest;
 import com.xebia.learningmanagement.entity.LearningPathEmployees;
 import com.xebia.learningmanagement.entity.User;
 import com.xebia.learningmanagement.exception.LearningPathException;
 import com.xebia.learningmanagement.exception.UsernameNotFoundException;
-import com.xebia.learningmanagement.repository.*;
+import com.xebia.learningmanagement.repository.LearningPathEmployeesRepository;
+import com.xebia.learningmanagement.repository.UserRepository;
 import com.xebia.learningmanagement.service.EmployeeLearningPathService;
-import com.xebia.learningmanagement.util.EmailSend;
 import com.xebia.learningmanagement.util.ErrorBank;
 import com.xebia.learningmanagement.util.MessageBank;
 import org.modelmapper.ModelMapper;
@@ -17,29 +18,14 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class EmployeeLearningPathServiceImpl implements EmployeeLearningPathService {
     @Autowired
-    LearningPathEmployeesRepository learningPathEmployeesRepository;
-
-    @Autowired
-    protected LearningPathRepository learningPathRepository;
-
-    @Autowired
-    protected DurationRepository durationRepository;
-
-    @Autowired
     protected UserRepository userRepository;
-
     @Autowired
-    protected CourseRepository courseRepository;
-
-    @Autowired
-    protected EmailSend emailSend;
-
+    LearningPathEmployeesRepository learningPathEmployeesRepository;
 
     /***
      *
@@ -51,21 +37,21 @@ public class EmployeeLearningPathServiceImpl implements EmployeeLearningPathServ
         List<String> employeeLearningpathids = null;
         LearningPathEmployees learningpathemployees = null;
         List<LearningPathEmployees> datalist = new ArrayList<>();
-        List deletedlearningpath=new ArrayList();
+        List deletedlearningpath = new ArrayList();
         try {
             employeeLearningpathids = (List) data.get("ids");
-            if(employeeLearningpathids!=null) {
+            if (employeeLearningpathids != null) {
                 for (int i = 0; i < employeeLearningpathids.size(); i++) {
                     String id = String.valueOf(employeeLearningpathids.get(i));
                     learningpathemployees = learningPathEmployeesRepository.findById(Long.valueOf(id)).orElse(null);
                     if (learningpathemployees != null) {
                         datalist.add(learningpathemployees);
                         deletedlearningpath.add(learningpathemployees.getLearningPath().getName());
-                    }else{
+                    } else {
                         throw new LearningPathException(MessageBank.NO_DATA_FOUND);
                     }
                 }
-            }else{
+            } else {
                 throw new LearningPathException(ErrorBank.ERROR_WHILE_DELETING);
             }
             learningPathEmployeesRepository.deleteAll(datalist);
@@ -74,15 +60,19 @@ public class EmployeeLearningPathServiceImpl implements EmployeeLearningPathServ
         }
     }
 
+    /***
+     * @param employeeEmail
+     * @return
+     * @throws LearningPathException
+     */
     @Override
-    public void getMyAssignedLearningPaths(EmployeeEmailRequest employeeEmail) throws LearningPathException {
-        ModelMapper modelMapper =new ModelMapper();
+    public List<EmployeeLearningPathStatisticsDto> getMyAssignedLearningPaths(EmployeeEmailRequest employeeEmail) throws LearningPathException {
+        ModelMapper modelMapper = new ModelMapper();
         User user = userRepository.findByUsername(employeeEmail.getEmployeeEmail()).orElseThrow(() -> new UsernameNotFoundException("UserEmail does not exist"));
         List<LearningPathEmployees> learningPathEmployees = learningPathEmployeesRepository.findAll().stream().filter(a -> a.getEmployee().equals(user)).collect(Collectors.toList());
-
+        List<EmployeeLearningPathStatisticsDto> employeeLearningPathStatistics = learningPathEmployees.stream().map(a -> modelMapper.map(a, EmployeeLearningPathStatisticsDto.class)).collect(Collectors.toList());
+        return employeeLearningPathStatistics;
     }
-
-
 
 
 }
