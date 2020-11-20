33d736bccb8a634c99010b44d6cbf4ab192fda16
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-20 13:14:53 +0530

    HttpStatus.NOT_FOUND added in UsernameNotFoundException

--- src/main/java/com/xebia/learningmanagement/exception/UsernameNotFoundException.java
+++ src/main/java/com/xebia/learningmanagement/exception/UsernameNotFoundException.java
@@ -1,5 +1,9 @@
 package com.xebia.learningmanagement.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UsernameNotFoundException extends RuntimeException {
     public UsernameNotFoundException(String message) {
         super(message);
