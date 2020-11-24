6bbf9d66130e4b821d6aacf71055ea7e2a902cc1
raghavsindhwani <rsindhwani_be16@thapar.edu>
2020-11-24 15:03:57 +0530

    responsive and ui changes

---
+++ src/views/Chart/Chartview/style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(theme => ({
+left:{
+width:"15px",
+backgroundColor:"white",
+borderRadius:"15px",
+margin:"0 0 198px 2100px"
+},
+right:{
+  margin:"0 138px 198px 0px",
+  width:"15px",
+  backgroundColor:"white",
+  borderRadius:"15px"
+}
+}));
