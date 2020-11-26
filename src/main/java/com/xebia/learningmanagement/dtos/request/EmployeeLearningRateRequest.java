f792430c139d04bec896bdf3e069ff516d81396e
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-27 00:36:35 +0530

    File as input

--- src/main/java/com/xebia/learningmanagement/dtos/request/EmployeeLearningRateRequest.java
+++ src/main/java/com/xebia/learningmanagement/dtos/request/EmployeeLearningRateRequest.java
@@ -2,10 +2,12 @@ package com.xebia.learningmanagement.dtos.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 public class EmployeeLearningRateRequest {
     private Integer percentCompleted;
     private Long learningPathEmployeeId;
+    private MultipartFile file;
 }
