e7bc0f2afb4ba2b1b21c00e9f8f13d8cd7e5af8d
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-27 03:25:35 +0530

    Add getPendingApprovals defination

--- src/main/java/com/xebia/learningmanagement/service/LearningPathService.java
+++ src/main/java/com/xebia/learningmanagement/service/LearningPathService.java
@@ -1,9 +1,12 @@
 package com.xebia.learningmanagement.service;
 
+import com.xebia.learningmanagement.dtos.ApprovalDto;
 import com.xebia.learningmanagement.dtos.LearningPathDto;
 import com.xebia.learningmanagement.dtos.ListOfLearningPathsAssignedByManagerDto;
 import com.xebia.learningmanagement.dtos.request.ManagerEmailRequest;
 import com.xebia.learningmanagement.exception.LearningPathException;
+
+import java.util.List;
 
 public interface LearningPathService {
 
@@ -16,4 +19,6 @@ public interface LearningPathService {
      * @throws LearningPathException
      */
     ListOfLearningPathsAssignedByManagerDto getAllAssignedLearningPath(ManagerEmailRequest managerEmail) throws LearningPathException;
+
+    List<ApprovalDto> getPendingApprovals(ManagerEmailRequest managerEmailRequest);
 }
