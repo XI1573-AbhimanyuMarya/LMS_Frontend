02963ab225eb676abd9b40cc9312408d19c9e8b7
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-11-27 14:01:38 +0530

    Request

---
+++ src/main/java/com/xebia/learningmanagement/dtos/request/LearningPathEmployeeApprovalRequest.java
@@ -0,0 +1,12 @@
+package com.xebia.learningmanagement.dtos.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LearningPathEmployeeApprovalRequest {
+    private Long learningPathEmployeeId;
+    private String status;
+
+}
