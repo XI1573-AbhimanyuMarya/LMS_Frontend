60a5cec4f633ee23bc5cb1a6c45d2fe09be8c22f
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-12-01 17:00:25 +0530

    Update IsExpired for courses

--- src/main/java/com/xebia/learningmanagement/repository/LearningPathEmployeesRepository.java
+++ src/main/java/com/xebia/learningmanagement/repository/LearningPathEmployeesRepository.java
@@ -3,6 +3,9 @@ package com.xebia.learningmanagement.repository;
 import com.xebia.learningmanagement.entity.LearningPathEmployees;
 import com.xebia.learningmanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,5 +13,9 @@ public interface LearningPathEmployeesRepository extends JpaRepository<LearningP
 
     List<LearningPathEmployees> findByLearningPathMadeBy(User user);
 
+    @Modifying
+    @Transactional
+    @Query(value = "Update public.learning_path_employees SET  is_expired=true WHERE ( now() >= learning_path.end_date )", nativeQuery = true)
+    void updateIsExpiredOfLearningPath();
 
 }
