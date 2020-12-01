9afe1b7ec55b9660241e40fc63aa1a1fcd293f81
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-12-01 16:59:10 +0530

    Competency Repository

---
+++ src/main/java/com/xebia/learningmanagement/repository/CompetencyRepository.java
@@ -0,0 +1,9 @@
+package com.xebia.learningmanagement.repository;
+
+import com.xebia.learningmanagement.entity.Competency;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompetencyRepository extends JpaRepository<Competency, Long> {
+}
