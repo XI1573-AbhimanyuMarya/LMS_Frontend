bc54d38b99c67b21fbfb3de9bc69fdbd8d2f3d7e
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-12-01 19:04:10 +0530

    find Learnig Path MadeBy Manager

--- src/main/java/com/xebia/learningmanagement/repository/LearningPathRepository.java
+++ src/main/java/com/xebia/learningmanagement/repository/LearningPathRepository.java
@@ -1,14 +1,18 @@
 package com.xebia.learningmanagement.repository;
 
 import com.xebia.learningmanagement.entity.LearningPath;
+import com.xebia.learningmanagement.entity.LearningPathEmployees;
+import com.xebia.learningmanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 public interface LearningPathRepository extends JpaRepository<LearningPath, Long> {
 
-
+    List<LearningPath> findByMadeBy(User user);
 }
