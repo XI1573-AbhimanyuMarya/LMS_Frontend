f43d89f3e1c3a89fc9f2c620eed4f44e85b73214
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-12-05 00:25:02 +0530

    Assign Learning Path Request

---
+++ src/main/java/com/xebia/learningmanagement/dtos/request/AssignLearningPathRequest.java
@@ -0,0 +1,22 @@
+package com.xebia.learningmanagement.dtos.request;
+
+
+import com.xebia.learningmanagement.entity.Duration;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Getter
+@Setter
+public class AssignLearningPathRequest {
+
+    @NotEmpty
+    private List<Long> learningPathIds;
+    @NotEmpty
+    private List<Long> employeeIds;
+    @NotBlank
+    private Duration duration;
+}
