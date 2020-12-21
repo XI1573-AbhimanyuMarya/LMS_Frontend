cf97057cb768c5a2829e6bc988d8fc3d4ae4ac36
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-12-22 03:13:39 +0530

    findByLearningPathEmployeeIdAndEmployeeId

--- src/main/java/com/xebia/learningmanagement/repository/CertificateRepository.java
+++ src/main/java/com/xebia/learningmanagement/repository/CertificateRepository.java
@@ -1,16 +1,13 @@
 package com.xebia.learningmanagement.repository;
 
 import com.xebia.learningmanagement.entity.Certificate;
-import com.xebia.learningmanagement.entity.CourseRating;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CertificateRepository extends JpaRepository<Certificate,Long> {
-    @Query("SELECT u FROM Certificate u WHERE u.learningPathEmployeeId = ?1 and u.employeeId = ?2")
-    public List<Certificate> getCertificateByLearningPathIdAndEmployeeId(long learningPathId, long employeeId);
+public interface CertificateRepository extends JpaRepository<Certificate, Long> {
+    public List<Certificate> findByLearningPathEmployeeIdAndEmployeeId(long learningPathId, long employeeId);
 
 }
