9a65f0a2e88b70ec91fd5e018eff62b70a1f73c6
Chanchal.Srivastava <chanchal.srivastava@xebia.com>
2020-12-21 13:03:50 +0530

    services changes for courses rating

---
+++ src/main/java/com/xebia/learningmanagement/dtos/request/CourseCompletedPercentRequest.java
@@ -0,0 +1,19 @@
+package com.xebia.learningmanagement.dtos.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+
+@Getter
+@Setter
+public class CourseCompletedPercentRequest {
+    @Min(value=1)
+   private long employeeId;
+    @Min(value=1)
+   private long learningPathId;
+    @Min(value=1)
+    private long courseId;
+    @Min(value=1)
+    private int percentCompleted;
+}
