f81c9d965ce9d70cb14729fbe4c009ae61f85803
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-12-23 00:57:38 +0530

    Validation constraint annotations added.

--- src/main/java/com/xebia/learningmanagement/dtos/request/LearningPathEmployeeApprovalRequest.java
+++ src/main/java/com/xebia/learningmanagement/dtos/request/LearningPathEmployeeApprovalRequest.java
@@ -3,11 +3,17 @@ package com.xebia.learningmanagement.dtos.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class LearningPathEmployeeApprovalRequest {
+    @NotNull
     private Long learningPathEmployeeId;
+    @NotBlank
     private String status;
+    @NotBlank
     private String reviewMessage;
 
 }
