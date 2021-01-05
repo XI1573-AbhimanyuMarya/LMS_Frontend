34a322084c5d91d35e93cd8898158a9a759e5dc1
Abhimanyu Marya <Abhimanyu.Marya@XEBIAINDIA.LOCAL>
2021-01-05 13:58:43 +0530

    Refactor

--- src/main/java/com/xebia/learningmanagement/dtos/response/UserResponse.java
+++ src/main/java/com/xebia/learningmanagement/dtos/response/UserResponse.java
@@ -1,25 +1,18 @@
 package com.xebia.learningmanagement.dtos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
     private String status;
     private String message;
-    private Payload payload;
 
-    public class Payload {
-        private List<?> objectList;
-
-        public List<?> getObjectList() {
-            return objectList;
-        }
-
-        public void setObjectList(List<?> objectList) {
-            this.objectList = objectList;
-        }
-    }
 }
