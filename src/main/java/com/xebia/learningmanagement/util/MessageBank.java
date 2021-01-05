aa19cc91b2560af92f04028e6f64aee13f34f2c0
Abhimanyu Marya <Abhimanyu.Marya@XEBIAINDIA.LOCAL>
2021-01-05 13:54:55 +0530

    Failure constant

--- src/main/java/com/xebia/learningmanagement/util/MessageBank.java
+++ src/main/java/com/xebia/learningmanagement/util/MessageBank.java
@@ -1,6 +1,13 @@
 package com.xebia.learningmanagement.util;
 
+
 public class MessageBank {
+    private MessageBank() {
+    }
+
     public static final String STATUS_SUCESS = "Success";
     public static final String NO_DATA_FOUND = "No Data Found For Given Key";
+    public static final String FAILURE = "failure";
+
+
 }
