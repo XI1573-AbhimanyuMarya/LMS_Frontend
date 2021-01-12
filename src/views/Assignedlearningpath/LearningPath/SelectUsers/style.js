c281c6bb6353ac22d3147501e05c9c1c3347ad4e
raghavsindhwani <rsindhwani_be16@thapar.edu>
2021-01-12 11:03:05 +0530

    discard popup and ui changes

--- src/views/Assignedlearningpath/LearningPath/SelectUsers/style.js
+++ src/views/Assignedlearningpath/LearningPath/SelectUsers/style.js
@@ -1,58 +1,65 @@
-import { makeStyles } from '@material-ui/core/styles';
-import { deepOrange, deepPurple, green, yellow, blueGrey, pink } from '@material-ui/core/colors';
-import { Autocomplete } from '@material-ui/lab';
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  deepOrange,
+  deepPurple,
+  green,
+  yellow,
+  blueGrey,
+  pink,
+} from "@material-ui/core/colors";
+import { Autocomplete } from "@material-ui/lab";
 
 export const useStyles = makeStyles((theme) => ({
-	searchField: {
-		width: '70%',
-		background: '#FFFFFF',
-		margin: theme.spacing(2),
-	},
-	box: {
-		display: 'flex',
-		width: 250,
-		cursor: 'pointer',
-		border: '2px solid #A9A9A9'
-	},
-	selected: {
-		display: 'flex',
-		width: 250,
-		cursor: 'pointer',
-		border: '2px solid #67B104'
-	},
-	orangeAvtar: {
-		color: theme.palette.getContrastText(deepOrange[500]),
-		backgroundColor: deepOrange[500],
-	},
-	purpleAvtar: {
-		color: theme.palette.getContrastText(deepPurple[500]),
-		backgroundColor: deepPurple[500],
-	},
-	greenAvtar: {
-		color: '#FFFFFFF',
-		backgroundColor: green[500],
-	},
-	yellowAvtar: {
-		color: '#FFFFFFF',
-		backgroundColor: yellow[500],
-	},
-	blueGreyAvtar: {
-		color: '#FFFFFFF',
-		backgroundColor: blueGrey[500],
-	},
-	pinkAvtar: {
-		color: '#FFFFFFF',
-		backgroundColor: pink[500],
-	},
-	checkIcon: {
-    color: '#67B104',
-    float: 'right',
-    margin:"0 0 0 -24px"
-	},
-	usersContainer: {
-		background: '#F1F3F7',
+  searchField: {
+    width: "70%",
+    background: "#FFFFFF",
+    margin: theme.spacing(2),
+  },
+  box: {
+    display: "flex",
+    width: 250,
+    cursor: "pointer",
+    border: "1px solid #A9A9A9",
+  },
+  selected: {
+    display: "flex",
+    width: 250,
+    cursor: "pointer",
+    border: "2px solid #67B104",
+  },
+  orangeAvtar: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+  purpleAvtar: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+  greenAvtar: {
+    color: "#FFFFFFF",
+    backgroundColor: green[500],
+  },
+  yellowAvtar: {
+    color: "#FFFFFFF",
+    backgroundColor: yellow[500],
+  },
+  blueGreyAvtar: {
+    color: "#FFFFFFF",
+    backgroundColor: blueGrey[500],
+  },
+  pinkAvtar: {
+    color: "#FFFFFFF",
+    backgroundColor: pink[500],
+  },
+  checkIcon: {
+    color: "#67B104",
+    float: "right",
+    margin: "0 0 0 -24px",
+  },
+  usersContainer: {
+    background: "#F1F3F7",
     borderRadius: "0.5%",
-    height:"50vh",
-    overflow:"auto"
-	},
+    height: "50vh",
+    overflow: "auto",
+  },
 }));
