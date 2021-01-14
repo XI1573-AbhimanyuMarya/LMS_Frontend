3b517357ef608d5fe67fd1293e373ef37559948e
raghavsindhwani <rsindhwani_be16@thapar.edu>
2021-01-14 17:23:18 +0530

    learner branch

---
+++ src/components/Table/LearnersTable/TableRow.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useStyles } from "./style";
+
+const TableRow = (props) => {
+  const classes = useStyles();
+  const { lp } = props;
+  return (
+    <tr className={classes.tblrow}>
+      <td>{lp.learnerName}</td>
+      <td>{lp.level}</td>
+      <td>{lp.startDate}</td>
+      <td>{lp.endDate}</td>
+      <td>{lp.percentCompleted}</td>
+    </tr>
+  );
+};
+
+export default TableRow;
