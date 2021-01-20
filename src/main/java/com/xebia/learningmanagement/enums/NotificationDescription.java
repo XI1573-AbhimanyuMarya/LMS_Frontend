6ce3b242ef1a6ed30a3e7aeffd15da55e02cf78b
Abhimanyu Marya <Abhimanyu.Marya@XEBIAINDIA.LOCAL>
2021-01-20 13:56:34 +0530

    LP_EXPIRING_IN_A_WEEK enums

--- src/main/java/com/xebia/learningmanagement/enums/NotificationDescription.java
+++ src/main/java/com/xebia/learningmanagement/enums/NotificationDescription.java
@@ -3,6 +3,7 @@ package com.xebia.learningmanagement.enums;
 public enum NotificationDescription {
     LP_ASSIGNED("A Learning path has been assigned to you by "), /* A Learning path has been assigned to you by MANAGER NAME  */
     LP_EXPIRED(" has been expired. Please ask to your manager for extension."), /* LEARNING PATH NAME has been deleted by your manager.  */
+    LP_EXPIRING_IN_A_WEEK(" Last One week to complete learning path : "), /* LEARNING PATH NAME has been deleted by your manager.  */
     LP_MODIFIED("A Learning path has been modified you can check the details."),
     LP_APPROVAL_REQUIRED(" has asked for your approval on learning path : "),  /*  EMPLOYEE NAME has asked for your approval on learning path  LEARNING PATH NAME   */
     LP_DELETED(" has been deleted by your manager."), /* A LEARNING PATH NAME has been deleted by your manager.  */
