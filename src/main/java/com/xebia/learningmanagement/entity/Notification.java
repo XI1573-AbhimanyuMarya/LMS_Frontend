b8c4a4c4dff7c1ee32dcb5d471d2e4cf56c8369e
Abhimanyu Marya <Abhimanyu.Marya@XEBIAINDIA.LOCAL>
2021-01-20 13:57:11 +0530

    replacing @Data with @Getter and @Setter

--- src/main/java/com/xebia/learningmanagement/entity/Notification.java
+++ src/main/java/com/xebia/learningmanagement/entity/Notification.java
@@ -1,14 +1,12 @@
 package com.xebia.learningmanagement.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Builder
 @NoArgsConstructor
