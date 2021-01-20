f659b22ec781f7b963c6ea01a807ba72b65403b5
Rajesh Pradhan <Rajesh.Pradhan@XEBIAINDIA.LOCAL>
2021-01-20 09:56:10 +0530

    Pagination in notification

--- src/store/actions/notificationActions.js
+++ src/store/actions/notificationActions.js
@@ -1,11 +1,15 @@
 import { actionTypes } from "../types";
 
-const fetchNotifications = (userId) => ({
+const fetchNotifications = (userId, pageNumber) => ({
   type: actionTypes.FETCH_NOTIFICATIONS,
-  payload: userId ,
+  payload: {userId, pageNumber} ,
 });
 const fetchNotificationCount = (userId) => ({
   type: actionTypes.FETCH_NOTIFICATIONSCOUNT,
+  payload: userId ,
+});
+const markAsRead = (userId) => ({
+  type: actionTypes.MARK_AS_READ,
   payload: userId ,
 });
 const eraseCount = () => ({ type: actionTypes.ERASE_COUNT });
@@ -13,5 +17,6 @@ const eraseCount = () => ({ type: actionTypes.ERASE_COUNT });
 export default {
   fetchNotifications,
   fetchNotificationCount,
+  markAsRead,
   eraseCount
 };
