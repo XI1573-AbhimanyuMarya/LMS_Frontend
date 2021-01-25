585e6961a7c967d0693a56daaae13889df8125e8
Abhimanyu Marya <Abhimanyu.Marya@XEBIAINDIA.LOCAL>
2021-01-25 12:40:59 +0530

    checkstyle

--- src/main/java/com/xebia/learningmanagement/service/impl/AdminServiceImpl.java
+++ src/main/java/com/xebia/learningmanagement/service/impl/AdminServiceImpl.java
@@ -15,7 +15,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
