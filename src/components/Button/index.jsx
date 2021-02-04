eba049311bdc05e2c56e0afae0b86f3965d276b9
Manan Chawla <mananchawla@Manans-MacBook-Air.local>
2021-02-04 14:22:50 +0530

    text change

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -12,20 +12,20 @@ import { useDispatch } from "react-redux";
 import Actions from "../../store/actions";
 
 
-export const SaveButton=(props)=>{
-  const {saveRateHandler}=props;
-  const classes=useStyles();
+export const SaveButton = (props) => {
+  const { saveRateHandler } = props;
+  const classes = useStyles();
 
   return (
-      <Button className={classes.savebtn} onClick={saveRateHandler} size="small" startIcon={<img src={GreenCheck} />}>
-        Save
-      </Button>
+    <Button className={classes.savebtn} onClick={saveRateHandler} size="small" startIcon={<img src={GreenCheck} />}>
+      Save
+    </Button>
   );
 }
 
-export const UploadButton=(props)=>{
-  const { onViewClick,discardHandler,changeImgHandler,uploadDocs }=props;
-  const classes=useStyles();
+export const UploadButton = (props) => {
+  const { onViewClick, discardHandler, changeImgHandler, uploadDocs } = props;
+  const classes = useStyles();
   return (
     <>
       <Button className={classes.uploadbtn} size="small" onClick={onViewClick}>
@@ -35,35 +35,35 @@ export const UploadButton=(props)=>{
   );
 }
 
-export const WaitForApprovalButton=()=>{
-  const classes=useStyles();
+export const WaitForApprovalButton = () => {
+  const classes = useStyles();
   return (
     <>
       <Button className={classes.completed}>{`100%`}</Button>
-      <Button className={classes.waitbtn}>{"Wait for approval"}</Button>
+      <Button className={classes.waitbtn}>{"Waiting for approval"}</Button>
     </>
   );
 }
 
-export const ApprovedButton=()=>{
+export const ApprovedButton = () => {
   return (
-      <DoneOutlineRoundedIcon />
+    <DoneOutlineRoundedIcon />
   );
 }
 
-export const BackButton=(props)=>{
-  const {backBtnHandler}=props;
-  const classes=useStyles();
+export const BackButton = (props) => {
+  const { backBtnHandler } = props;
+  const classes = useStyles();
   return (
-    <Button size="small" onClick={backBtnHandler} className={classes.bckbtn} startIcon={<img src={ArrowBackIos}/>}>
+    <Button size="small" onClick={backBtnHandler} className={classes.bckbtn} startIcon={<img src={ArrowBackIos} />}>
       Back
     </Button>
   );
 }
 
-export const ViewButton=(props)=>{
-  const {onButtonClick}=props;
-  const classes=useStyles();
+export const ViewButton = (props) => {
+  const { onButtonClick } = props;
+  const classes = useStyles();
   return (
     <Button variant="outlined" onClick={onButtonClick} size="small" className={classes.viewbtn}>
       {"View"}
@@ -71,9 +71,9 @@ export const ViewButton=(props)=>{
   );
 }
 
-export const LearningRateButton=(props)=>{
-  const {percentCompleted}=props;
-  const classes=useStyles();
+export const LearningRateButton = (props) => {
+  const { percentCompleted } = props;
+  const classes = useStyles();
   return (
     <Button variant="outlined" size="small" className={classes.avglearningrate}>
       {`${percentCompleted}%`}
@@ -81,42 +81,42 @@ export const LearningRateButton=(props)=>{
   );
 }
 
-export const CourseAssignButton=(props)=>{
-  const {onButtonClick,assignLp, count}=props;
-  const classes=useStyles();
-  
+export const CourseAssignButton = (props) => {
+  const { onButtonClick, assignLp, count } = props;
+  const classes = useStyles();
+
   return (
     <Button size="small" variant="outlined" className={classes.cardfooterbtn} onClick={onButtonClick}>
       {count}
-      {assignLp && <img src={ArrowForwardIos} className={classes.arrowfwbtn}/> }
+      {assignLp && <img src={ArrowForwardIos} className={classes.arrowfwbtn} />}
     </Button>
   );
 }
 
-export const ApproveButton=(props)=>{
-  const {onViewClickApprove}=props;
-  const classes=useStyles();
+export const ApproveButton = (props) => {
+  const { onViewClickApprove } = props;
+  const classes = useStyles();
   return (
     <Button className={classes.approve} onClick={onViewClickApprove}>Approve</Button>
   );
 }
 
-export const RejectButton=(props)=>{
-  const {onViewClick}=props;
-  const classes=useStyles();
+export const RejectButton = (props) => {
+  const { onViewClick } = props;
+  const classes = useStyles();
   return (
     <Button className={classes.reject} onClick={onViewClick}>Reject</Button>
   );
 }
 
-export const CompletedButton=()=>{
-  const classes=useStyles();
+export const CompletedButton = () => {
+  const classes = useStyles();
   return (
-    <Button className={classes.completed}>{`100%`}<DoneRoundedIcon className={classes.doneroundedicon}/></Button>
+    <Button className={classes.completed}>{`100%`}<DoneRoundedIcon className={classes.doneroundedicon} /></Button>
   );
 }
 
-export const ViewDetails=(props)=>{
+export const ViewDetails = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const onViewDetailsClick = () => {
